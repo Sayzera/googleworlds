b88fc27bdfb6c3430bf2226b0c0813a06607e873
Sezer <sezer.boluk@niltekyazilim.com.tr>
2025-01-21 21:26:03 +0300

    latests

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -51,7 +51,6 @@ import {
   UploadIcon,
 } from "lucide-react";
 import { useState } from "react";
-import TextAlign from "@tiptap/extension-text-align";
 
 interface ToolbarButtonProps {
   onClick?(): void;
@@ -149,52 +148,72 @@ const FontSizeButton = () => {
 
   const updateFontSize = (newSize: string) => {
     const size = parseInt(newSize);
-    if(!isNaN(size) && size > 0) {
+    if (!isNaN(size) && size > 0) {
       editor?.chain().focus().setFontSize(`${size}px`).run();
       setFontSize(newSize);
       setInputValue(newSize);
       setIsEditing(false);
     }
-  }
+  };
 
-  const handleInputChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
-  }
+  };
 
   const handleInputBlur = () => {
     updateFontSize(inputValue);
-  }
+  };
 
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>)  => {
-    if(e.key === 'Enter') {
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
       e.preventDefault();
       updateFontSize(inputValue);
-      editor?.commands.focus()
+      editor?.commands.focus();
     }
-  }
-
+  };
 
   const increment = () => {
     const newSize = parseInt(fontSize) + 1;
     updateFontSize(newSize.toString());
-  }
+  };
 
-  
   const decrement = () => {
     const newSize = parseInt(fontSize) - 1;
-    if(newSize > 0) {
+    if (newSize > 0) {
       updateFontSize(newSize.toString());
     }
-  }
-
+  };
 
   return (
     <div className="flex items-center gap-x-0.5">
-        <button className="h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm">
-        <MinusIcon className="size-4"  />
+      <button
+        onClick={decrement}
+        className="h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+      >
+        <MinusIcon className="size-4" />
       </button>
+      {isEditing ? (
+        <input type="text" 
+          value={inputValue}
+          onChange={handleInputChange}
+          onBlur={handleInputBlur}
+          onKeyDown={handleKeyDown}
+          className="h-7 w-10 text-sm text-center rounded-sm border border-neutral-400 hover:bg-neutral-200/80 bg-transparent"
+
+        />
+      ) : (
+        <button
+          onClick={() => {
+            setIsEditing(true)
+            setFontSize(currentFontSize);
+          }}
+          className="h-7 w-10 text-sm text-center rounded-sm border border-neutral-400 hover:bg-neutral-200/80"
+        >
+          <span className="truncate">{fontSize}</span>
+        </button>
+      )}
     </div>
-  )
+  );
 };
 
 const AlignButton = () => {
@@ -313,7 +332,7 @@ const ImageButton = () => {
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <button className="h-7 min-w-7 shrink-0 flex items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm">
-            <ImageIcon className="ml-2 size-4 shrink-0" />
+            <ImageIcon className="size-4 shrink-0" />
           </button>
         </DropdownMenuTrigger>
         <DropdownMenuContent className="p-2.5 flex items-center gap-x-2">
@@ -375,7 +394,7 @@ const LinkButton = () => {
           }}
           className="h-7 min-w-7 shrink-0 flex items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
         >
-          <Link2Icon className="ml-2 size-4 shrink-0" />
+          <Link2Icon className=" size-4 shrink-0" />
         </button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="p-2.5 flex items-center gap-x-2">
